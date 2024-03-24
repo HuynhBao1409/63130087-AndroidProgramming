d0b2de26079c57dff7c83e4e1f09ed4abbf98ed1
HuynhGiaBao1409 <giabao1409203@gmail.com>
2024-03-24 10:45:52 +0700

    Câu2_Them DK

--- Cau2_AppDoiNhietDo/app/src/main/java/ntu/MSSV_63130087/MainActivity.java
+++ Cau2_AppDoiNhietDo/app/src/main/java/ntu/MSSV_63130087/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edtF,edtC;
@@ -25,16 +26,29 @@ public class MainActivity extends AppCompatActivity {
         btnF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Ktra dieu kien
+                String Cm = edtC.getText().toString();
+                if (Cm.isEmpty()) {//Neu bo trong thi xuat ra thong bao
+                    Toast.makeText(MainActivity.this, "Vui lòng nhập giá trị C", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 //Chuyen kieu dlieu tu String sang int
                 int C = Integer.parseInt(edtC.getText().toString());
                 Double F = C * 1.8 + 32;
-                edtF.setText(F+"");
+                edtF.setText(F+"");//xóa giá trị của F để hiện thị kết quả mới
             }
         });
         //Chuyen Doi Tu C sang F
         btnC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Ktra dieu kien
+                String Fm = edtF.getText().toString();
+                if (Fm.isEmpty()) {//Neu bo trong thi xuat ra thong bao
+                    Toast.makeText(MainActivity.this, "Vui lòng nhập giá trị F", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //Chuyen kieu dlieu tu String sang int
                 int F = Integer.parseInt(edtF.getText().toString());
                 Double C = (F-32)/1.8;
                 edtC.setText(C+"");
@@ -44,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         btnClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Truyền dữ liệu vào chỗ nhập một chuỗi rỗng để xóa bỏ giá trị hiện tại
                 edtC.setText("");
                 edtF.setText("");
             }
