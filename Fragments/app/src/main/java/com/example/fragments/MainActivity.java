0814e0507877a08bc294b69400c8db62e4fd2555
HuynhGiaBao1409 <giabao1409203@gmail.com>
2024-04-19 19:07:27 +0700

    ,

--- Fragments/app/src/main/java/com/example/fragments/MainActivity.java
+++ Fragments/app/src/main/java/com/example/fragments/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.fragments;
 
+import static android.app.PendingIntent.getActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        return inflater.inflate(R.layout.fragment_2, container, false);
 //        FragmentManager fragmentManager = getSupportFragmentManager();
 //        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 //
@@ -28,5 +34,19 @@ public class MainActivity extends AppCompatActivity {
 //            fragmentTransaction.replace(android.R.id.content, fragment2);
 //        }
 //        fragmentTransaction.commit();
-    }
+        @Override
+        public void onStart() {
+            super.onStart();
+            Button btnGetText = (Button) getActivity().findViewById(R.id.btnGetText); //---Button view---
+            btnGetText.setOnClickListener(new View.OnClickListener() { //---Button click--
+                public void onClick(View v) {
+
+                    TextView lbl = (TextView) getActivity().findViewById(R.id.fragment1);
+                    TextView statusarea = (TextView) getActivity().findViewById(R.id.lblStatus);
+                    statusarea.setText("Fragment#1's text shows: " + lbl.getText());
+                }
+            });
+
+        }
+        }
 }
